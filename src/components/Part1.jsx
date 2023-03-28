bbf3fa1bbd220b7146d9e098594c74dec4dfdd1d
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-03-28 16:59:52 -0600

    Se añadieron los tipos de fuentes

--- src/components/Part1.jsx
+++ src/components/Part1.jsx
@@ -17,7 +17,7 @@ export default function Part1() {
       <div className="w-full h-1/10"></div>
       <section className="bg_Portada w-full h-9/10">
         <div className='relative top-1/2 left-20 text-white txtXl'>
-          <p className=''>Soy Josue Guzman</p>
+          <p className='txtSecondary txtXl'>Soy Josue Guzman</p>
           <p className=''>Desarrollador Web</p>
           <button className=''>ImAButton</button>
         </div>
