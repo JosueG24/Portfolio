50b859ad39e20bcfe2b5adc8db6727d214f804bf
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-05-02 11:00:55 -0600

    Se implementaron varias mejoras a los diseños responsive de varias secciones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,22 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-    <section id="Contacto" className='scrollEfect txtMd bg-c_Ebony text-c_Mischka w-full h-1/10 flex'>
-        <div className='w-4/5 h-full flexAllCenter'>
-          <a className='h-2/3 w-[calc(4%)] mx-2 hover:Lshadow transition-all' href=''><img src='./icons/Github-white.png'/></a>
-          <a className='h-2/3 w-[calc(4%)] mx-2 hover:Lshadow transition-all' href=''><img src='./icons/linkedin.png'/></a>
-          <a className='h-2/3 w-[calc(4%)] mx-2 hover:Lshadow transition-all' href=''><img src='./icons/Gmail.png'/></a>
-          <a className='h-2/3 w-[calc(4%)] mx-2 hover:Lshadow transition-all' href=''><img src='./icons/Whatsapp.png'/></a>
+    <section id="Contacto" className='scrollEfect txtMd bg-c_Ebony text-c_Mischka w-full h-1/10 flex px-6'>
+        <div className='w-4/5 h-full flexAllCenter justify-start'>
+          <a className='h-2/3 w-[calc(11%)] mx-2 hover:Lshadow transition-all flexAllCenter
+          md:h-2/3 md:w-[calc(7%)] lg:h-2/3 lg:w-[calc(4%)]' href='/'><img src='./icons/Github-white.png' alt=''/></a>
+
+          <a className='h-2/3 w-[calc(11%)] mx-2 hover:Lshadow transition-all flexAllCenter
+          md:h-2/3 md:w-[calc(7%)] lg:h-2/3 lg:w-[calc(4%)]' href='/'><img src='./icons/linkedin.png' alt=''/></a>
+
+          <a className='h-2/3 w-[calc(11%)] mx-2 hover:Lshadow transition-all flexAllCenter
+          md:h-2/3 md:w-[calc(7%)] lg:h-2/3 lg:w-[calc(4%)]' href='/'><img src='./icons/Gmail.png' alt=''/></a>
+
+          <a className='h-2/3 w-[calc(11%)] mx-2 hover:Lshadow transition-all flexAllCenter
+          md:h-2/3 md:w-[calc(7%)] lg:h-2/3 lg:w-[calc(4%)]' href='/'><img src='./icons/Whatsapp.png' alt=''/></a>
         </div>
-        <div className='w-1/5 h-full flexAllCenter flex-col'>
+        <div className='w-3/10 h-full flexAllCenter flex-col 
+        md:w-1/5'>
           <p className="">Josué Guzman - 2023</p>
           <p className=''>Gracias por ver</p>
         </div>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,53 @@
 import React from 'react'
 
 export default function Services() {
+
+  return (
+    <section id='Services' className='scrollEfect txtMd p-5 bg-c_Mischka text-c_Ebony w-full h-[calc(90vh-30px)] border-b-2 border-c_Ebony
+    md:h-[calc(90vh)]'>
+      <p className='w-full h-1/10 flexAllCenter txtXl txtSecondary'>Servicios</p>
+      <div className='flexAllCenter flex-wrap w-full h-9/10'>
+
+        <div className='margins outline-c_Ebony w-[calc(45%)] max-w-[calc(460px)] h-[calc(45%)] mx-[calc(1%)] my-[calc(1%)] rounded-lg overflow-hidden'>
+          <div className='bgServ1 w-full h-full '></div>
+          <div className='text-c_Mischka w-full h-3/10 bg-c_Opaque relative bottom-[calc(25%)]
+          md:h-3/10 md:bottom-[calc(20%)] lg:h-3/10 lg:bottom-[calc(25%)]'>
+            <p className='txtLg w-full py-2 text-center textSecondary'>Diseño y desarrollo de sitios web</p>
+            <p className='p-4'></p>
+          </div>
+        </div>
+
+        {/* <div className='margins outline-c_Ebony w-[calc(45%)] max-w-[calc(460px)] h-[calc(45%)] mx-[calc(1%)] my-[calc(1%)] rounded-lg overflow-hidden'>
+          <div className='bgServ2 w-full h-full '></div>
+          <div className='text-c_Mischka w-full h-3/10 bg-c_Opaque relative bottom-[calc(25%)]
+          md:h-3/10 md:bottom-[calc(30%)] lg:h-4/10 lg:bottom-[calc(35%)]'>
+            <p className='txtLg w-full py-2 text-center textSecondary'>Stack Mern</p>
+            <p className='p-4'></p>
+          </div>
+        </div> */}
+
+        <div className='margins outline-c_Ebony w-[calc(45%)] max-w-[calc(460px)] h-[calc(45%)] mx-[calc(1%)] my-[calc(1%)] rounded-lg overflow-hidden'>
+          <div className='bgServ3 w-full h-full '></div>
+          <div className='text-c_Mischka w-full h-3/10 bg-c_Opaque relative bottom-[calc(25%)]
+          md:h-3/10 md:bottom-[calc(30%)] lg:h-4/10 lg:bottom-[calc(35%)]'>
+            <p className='txtLg w-full py-2 text-center textSecondary'>Mantenimiento y actualización de sitios web</p>
+            <p className='p-4'></p>
+          </div>
+        </div>
+
+        <div className='margins outline-c_Ebony w-[calc(45%)] max-w-[calc(460px)] h-[calc(45%)] mx-[calc(1%)] my-[calc(1%)] rounded-lg overflow-hidden'>
+          <div className='bgServ4 w-full h-full '></div>
+          <div className='text-c_Mischka w-full h-3/10 bg-c_Opaque relative bottom-[calc(25%)]
+          md:h-3/10 md:bottom-[calc(30%)] lg:h-4/10 lg:bottom-[calc(35%)]'>
+            <p className='txtLg w-full py-2 text-center textSecondary'>Consultoría y asesoramiento en tecnología web</p>
+            <p className='p-4'></p>
+          </div>
+        </div>
+
+      </div>
+  </section>
+  )
+
   return (
     <section id='Services' className='scrollEfect txtMd p-5 bg-c_Mischka text-c_Ebony w-full h-[calc(90vh-30px)] border-b-2 border-c_Ebony
     md:h-[calc(90vh)]'>
