2c7234c2b33c5ca85b10310f5429372c566ca972
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-05-07 20:19:03 -0600

    Se actualizó la información de las zonas de ProjectCard

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -27,10 +27,10 @@ export default function Portfolio() {
             ImgPrinc="bgPort2"
             Collection={["/ProjectSrc/ProjectSPGF/img1.jpeg","/ProjectSrc/ProjectSPGF/img2.jpeg","/ProjectSrc/ProjectSPGF/img3.jpeg","/ProjectSrc/ProjectSPGF/img4.jpeg","/ProjectSrc/ProjectSPGF/img5.jpeg"]} 
             title="SuperGames Frontend"
-            description="Este sitio web fue creado para mostrar mis proyectos y las tecnologías con las que trabajo, también con la intención de poder ser contactado por personas interesadas en lo que hago." 
-            conoc="En el desarrollo de este proyecto se utilizó: Principalmente un alto nivel de Css. Buen manejo de componentes y Hoocks de react. Implementación del formulario de contacto con API de Gmail. Despliegue del sitio web en hosting y adquisición de un dominio propio."
-            tecnos={["React","NextJs","Vercel","emailJS","Tsparticles","SwiperJs"]}
-            link={["https://www.jguzman.tk/","https://github.com/JosueElutiche24/PortafolioProfesional"]}
+            description="Un sitio en el que compites contra otros jugadores por romper el record del mejor jugador. Puedes competir en los juegos de Memoria, BuscaMinas y Snake. Crea tu perfil y nosotros nos encargamos de almacenar tus puntuaciones."
+            conoc="Para desarrollar este proyecto se utilizó conocimientos en maquetacion de itios web utilizando React y Tailwind, mucho ingenio para poder desarrollar los juegos con los componentes que proporciona react y POO para el desarrollo de la lógica. También se utilizó axios para realizar las peticiones al servidor."
+            tecnos={["NodeJs","React","NextJs","Vercel","Axios","Tailwind","Email-Js","Dall-e"]}
+            link={["https://sp-games2.jguzman.tk/login","https://github.com/JosueG24/SuperGamesFrontend"]}
           />
         </div>
         {/* Third icon */}
@@ -49,12 +49,12 @@ export default function Portfolio() {
         <div className="scrollEfectIII w-[calc(75%)] h-[calc(65vh)] my-[calc(5vh)]">
           <PortfolioCard className="scrollEfectIII"
             ImgPrinc="bgPort1"
-            Collection={["/Project2/img1.jpeg","/Project2/img2.jpeg","/Project2/img3.jpeg","/Project2/img4.jpeg","/Project2/img5.jpeg"]} 
+            Collection={["/ProjectSrc/ProjectPortfolio/img1.jpeg"]} 
             title="Portafolio"
-            description="Este sitio web fue creado para mostrar mis proyectos y las tecnologías con las que trabajo, también con la intención de poder ser contactado por personas interesadas en lo que hago." 
+            description="Este sitio web fue creado para mostrar mis conocimientos y habilidades, así como el valor que puedo aportar a equipos de trabajo de desarrollo web." 
             conoc="En el desarrollo de este proyecto se utilizó: Principalmente un alto nivel de Css. Buen manejo de componentes y Hoocks de react. Implementación del formulario de contacto con API de Gmail. Despliegue del sitio web en hosting y adquisición de un dominio propio."
-            tecnos={["React","NextJs","Tsparticles","emailJS","Vercel","SwiperJs"]}
-            link={["https://www.jguzman.tk/","https://github.com/JosueElutiche24/PortafolioProfesional"]}
+            tecnos={["NodeJs","React","emailJS","Vercel","Tailwind"]}
+            link={["https://www.jguzman.tk/","https://github.com/JosueG24/myNewPortfolio"]}
           />
         </div>
 
